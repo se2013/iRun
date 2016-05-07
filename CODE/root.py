5aad1dc3a09ced25dd0ec8e8dd94621e9ccfa4c7
se2013 <378344082@qq.com>
2016-05-07 21:05:44 +0800

    CODE

--- CODE/root.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python 
-# -*- coding: utf-8 -*-
-
-import os
-import sys
-
-sys.path.append(os.path.join(sys.path[0], 'view'))
-sys.path.append(os.path.join(sys.path[0], 'control'))
-sys.path.append(os.path.join(sys.path[0], 'model'))
