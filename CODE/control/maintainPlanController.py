c57002399eb70d648ecd80de3a29314debb5f167
se2013 <378344082@qq.com>
2016-05-20 11:43:13 +0800

    Update maintainPlanController.py

--- CODE/control/maintainPlanController.py
+++ CODE/control/maintainPlanController.py
@@ -2,3 +2,31 @@
 # -*- coding: utf-8 -*-
 
 from modelRoot import *
+from dbControl import *
+from Plan import *
+
+class maintainPlanController(object):
+    def __init__(self):
+    	pass
+
+    def getAllPlan(self):
+    	db = dbControl()
+    	result = db.getAllPlan()
+    	return result
+
+    def __deleteCollectPlanByPlanId(self, planid):
+    	db = dbControl()
+    	db.deleteCollectPlanByPlanId(planid)
+
+    def __deleteJoinPlanByPlanId(self, planid):
+    	db = dbControl()
+    	result = db.deleteJoinPlanByPlanId(planid)
+
+    def __deleteCreatePlanByPlanId(self, planid):
+    	db = dbControl()
+    	result = db.deleteCreatePlanByPlanId(planid)
+
+    def deletePlan(self, planid):
+    	self.__deleteCollectPlanByPlanId(planid)
+    	self.__deleteJoinPlanByPlanId(planid)
+    	self.__deleteCreatePlanByPlanId(planid)
