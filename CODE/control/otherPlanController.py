89f33f2cca1bc56e594b4ecfcb0865f2804771be
se2013 <378344082@qq.com>
2016-05-20 11:46:43 +0800

    Update otherPlanController.py

--- CODE/control/otherPlanController.py
+++ CODE/control/otherPlanController.py
@@ -2,3 +2,22 @@
 # -*- coding: utf-8 -*-
 
 from modelRoot import *
+from dbControl import *
+
+class otherPlanController(object):
+    def __init__(self, account, password):
+        self.__user = User(account, password)
+
+    def getPlanByOther(self):
+    	db = dbControl()
+        result = db.getOtherPlanByUserAccount(self.__user.getAccount())
+        return result
+
+    def joinPlan(self, planid):
+    	db = dbControl()
+        db.joinPlan(self.__user.getAccount(), planid)
+
+    def collectPlan(self, planid):
+    	db = dbControl()
+        result = db.collectPlan(self.__user.getAccount(), planid)
+        return result
