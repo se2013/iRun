b5d727e404da987b846d014e890d8f5785933fbe
se2013 <378344082@qq.com>
2016-05-20 11:38:14 +0800

    Update aboutMeController.py

--- CODE/control/aboutMeController.py
+++ CODE/control/aboutMeController.py
@@ -2,3 +2,24 @@
 # -*- coding: utf-8 -*-
 
 from modelRoot import *
+from dbControl import *
+from User import *
+
+class AboutMeController(object):
+    def __init__(self, account, password):
+        self.__user = User(account, password)
+
+    def getCreatePlanByAboutMe(self):
+    	db = dbControl()
+        result = db.getCreatePlanByUserAccount(self.__user.getAccount())
+        return result
+
+    def getCollectPlanByAboutMe(self):
+    	db = dbControl()
+        result = db.getCollectPlanByUserAccount(self.__user.getAccount())
+        return result
+
+    def getJoinPlanByAboutMe(self):
+    	db = dbControl()
+    	result = db.getJoinPlanByUserAccount(self.__user.getAccount())
+    	return result
