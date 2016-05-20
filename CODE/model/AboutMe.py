0b074b2c75db17e95f4527644ccdef3c957b97a5
se2013 <378344082@qq.com>
2016-05-20 11:58:01 +0800

    Update AboutMe.py

---
+++ CODE/model/AboutMe.py
@@ -0,0 +1,29 @@
+#coding=utf-8
+
+class AboutMe():
+	def __init__(self, Id, User_Id, Plan_Id):
+		self.id = Id
+		self.user_id = User_Id
+		self.plan_id = Plan_Id
+
+	def getId(self):
+		return self.id
+
+	def setId(self,Id):
+		self.id = Id
+						
+	def getUserId(self):
+		return self.user_id
+
+	def setUserId(self,User_Id):
+		self.user_id = User_Id
+			
+	def getPlanId(self):
+		return plan_id
+		
+	def setPlanId(self,Plan_Id):
+		self.plan_id = Plan_Id
+	
+	@staticmethod
+	def creatAboutMe(Id, User_Id, Plan_Id):
+		return AboutMe(Id, User_Id, Plan_Id)
