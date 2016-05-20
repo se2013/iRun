42aca9959b5cb4a1dc722a12f6ce7cbd14f7ff86
se2013 <378344082@qq.com>
2016-05-20 11:44:26 +0800

    Update maintainUserController.py

--- CODE/control/maintainUserController.py
+++ CODE/control/maintainUserController.py
@@ -2,3 +2,38 @@
 # -*- coding: utf-8 -*-
 
 from modelRoot import *
+from dbControl import *
+from User import *
+from Admin import *
+from Plan import *
+
+class maintainUserController(object):
+    def __init__(self):
+    	pass
+
+    def getAllUser(self):
+    	db = dbControl()
+    	result = db.getAllUser()
+    	return result
+
+    def __deleteCreatePlanByAccount(self, account):
+    	db = dbControl()
+    	db.deleteCreatePlanByUserAccount(account)
+
+    def __deleteJoinPlanByAccount(self, account):
+    	db = dbControl()
+    	db.deleteJoinPlanByUserAccount(account)
+
+    def __deleteCollectPlanByAccount(self, account):
+    	db = dbControl()
+    	db.deleteCollectPlanByUserAccount(account)
+
+    def __deleteUserByAccount(self, account):
+    	db = dbControl()
+    	db.delUser(account)
+
+    def deleteUser(self, account):
+    	self.__deleteCreatePlanByAccount(account)
+    	self.__deleteJoinPlanByAccount(account)
+    	self.__deleteCollectPlanByAccount(account)
+    	self.__deleteUserByAccount(account)
