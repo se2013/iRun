8d494afdedb0dc800bf80578dce65419125fde12
se2013 <378344082@qq.com>
2016-05-20 11:41:22 +0800

    Update loginController.py

--- CODE/control/loginController.py
+++ CODE/control/loginController.py
@@ -2,27 +2,49 @@
 # -*- coding: utf-8 -*-
 
 from modelRoot import *
+from dbControl import *
+from User import *
+from Admin import *
 
-class loginController():
-    def checkAccount(self, account):
-    	pass
+class loginController(object):
+    def __init__(self, account, password):
+        self.__account = account
+        self.__password = password
 
-    def checkPassword(self, password):
-    	pass
+    def __checkAccount(self):
+    	db = dbControl()
+        result = db.getUserByAccount(self.__account)
+        if len(result) == 0:
+            return False
+        return True
+
+    def __checkPassword(self):
+    	db = dbControl()
+        result = db.getUserByAccount(self.__account)
+        if result[0][3] != self.__password:
+            return False
+        return True
 
     def getUserByAccount(self, account):
-    	pass
+    	db = dbControl()
+        result = db.getUserByAccount(account)
+    	user = User(result[0][1], result[0][2], result[0][3])
+        return user
 
     def getAdminByAccount(self, account):
-    	pass
+    	db = dbControl()
+        result = db.getAdminByAccount(account)
+    	admin = Admin(result[0][1], result[0][2], result[0][3])
+        return admin
 
     def login(self):
-    	t = User()
-    	pass
+        return True
 
     def check(self):
-    	pass
-
-    def test(self):
-    	t = User(1, '123', '123', '123')
-    	print 123
+        if self.__checkAccount() != True:
+            print 'Account not exists!'
+            return False
+        if self.__checkPassword() != True:
+            print 'Password is wrong!'
+            return False
+        return True
