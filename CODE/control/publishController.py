2d8c3cbab6f963a72ace599d57c66a744ad34ba7
se2013 <378344082@qq.com>
2016-05-20 11:47:18 +0800

    Update publishController.py

--- CODE/control/publishController.py
+++ CODE/control/publishController.py
@@ -2,3 +2,11 @@
 # -*- coding: utf-8 -*-
 
 from modelRoot import *
+from dbControl import *
+
+class publishController(object):
+    def __init__(self, time, location, contact, create_user, publish_time, tips):
+        self.__plan = Plan(time, location, contact, create_user, publish_time, tips)
+
+    def insertPlan(self):
+        self.__plan.createPlan()
